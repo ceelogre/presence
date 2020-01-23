3949f1517bdc566b05bb2fc5b4085239dad1358b
ceelogre <sam.nishimwe@yahoo.com>
2020-01-23 23:39:44 +0200

    Attempt to build output on Heroku

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDom from'react-dom'
 
 function countDown () {
-    const remainingTime = new Date('jan 23, 2020 23:59:59') - new Date()
-    let timeout = remainingTime
+    let timeout = new Date('Jan 25, 2020 23:59:59')
     const element = (
         <div style= {{ margin: "0 auto", width: 400, paddingTop: 200, textAlign: "justify" }}>
             <h1>I'm working</h1>
-            <h2>This ugly page will be off in: </h2>
-            <h3> { new Date(timeout).toLocaleTimeString() } </h3>
+            <h2>This ugly page will be off at: </h2>
+            <h3> { timeout.toString() } </h3>
         </div>
     )
 
