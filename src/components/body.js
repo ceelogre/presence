01b6935fdb6e42987a7968513ed2fa8d82d1805c
ceelogre <sam.nishimwe@yahoo.com>
2020-04-09 23:18:29 +0200

    Test toggling states

--- src/components/body.js
+++ src/components/body.js
@@ -1,13 +1,24 @@
 import React  from 'react'
-import panda from '../assets/images/pandaTree.jpeg'
 
 class Body extends React.Component {
+    constructor(props) {
+        super(props)
+        this.toggleView = this.toggleView.bind(this)
+        this.state = ({
+            isVisible: 'hidden'
+        })
+    }
+    toggleView () {
+        this.setState((state) => ({
+            isVisible: this.state.isVisible == 'hidden' ? 'visible': 'hidden'
+            })
+        )
+    }
     render () {
         return (
             <div className="body">
-                <div className="coming-soon">
-                    <img src= {panda} />
-                </div>
+                <button onClick = { this.toggleView }>I fall apart</button>
+                <h2 style = {{ visibility: this.state.isVisible }}>Antidote</h2>
             </div>
         )
     }
