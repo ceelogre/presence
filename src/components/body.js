081e7ab20cc27a97158247f1308aa1d3af26e7dd
ceelogre <sam.nishimwe@yahoo.com>
2020-04-11 10:32:38 +0200

    Add loading spinner WIP

--- src/components/body.js
+++ src/components/body.js
@@ -1,11 +1,12 @@
 import React  from 'react'
+import '../stylesheets/components/body.scss'
 
 class Body extends React.Component {
     constructor(props) {
         super(props)
         this.toggleView = this.toggleView.bind(this)
         this.state = ({
-            isVisible: 'hidden'
+            isVisible: 'visible'
         })
     }
     toggleView () {
@@ -17,8 +18,19 @@ class Body extends React.Component {
     render () {
         return (
             <div className="body">
-                <button onClick = { this.toggleView }>I fall apart</button>
-                <h2 style = {{ visibility: this.state.isVisible }}>Antidote</h2>
+                {/* <button onClick = { this.toggleView }>I fall apart</button> */}
+                {/* <div  className = 'loading-animation' style = {{ visibility: this.state.isVisible }}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div> */}
+                <div className = 'svg-spinner'>
+                    <svg height = '100' width = '100'>
+                        <circle cx = '50' cy = '50' r = '45' fill = 'teal'>
+                            <animate attributeName = 'r' values='0; 10; 30; 10; 0' dur='10s' repeatCount = 'indefinite' />
+                        </circle>
+                    </svg>
+                </div>
             </div>
         )
     }
