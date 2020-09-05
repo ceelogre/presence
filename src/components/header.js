e2f559d2a835365d5878c1718e5dcc01f063e438
ceelogre <sam.nishimwe@yahoo.com>
2020-09-05 23:17:04 +0200

    ch: update dependencies
    ch: Configure CSR routing properly
    
    ch: Bump up dependencies

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Layout, Menu, Anchor } from 'antd'
-
-const { Link } = Anchor
+import { Layout, Menu} from 'antd'
+import { Link } from 'react-router-dom'
 
 import '../stylesheets/components/header.scss'
 
@@ -14,7 +13,7 @@ class Header_ extends React.Component {
                 <div className="logo"/>
                 <Menu theme="dark" mode="horizontal" style={{textAlign: 'center' }}>
                     <Menu.Item key="1">
-                        <a  href="/thoughts" title="Thoughts" >THOUGHTS</a>
+                        <Link  to="/thoughts" title="Thoughts" >THOUGHTS</Link>
                     </Menu.Item>
                     <Menu.Item key="2">
                         <a href="/work" title="Work" >WORK</a>
