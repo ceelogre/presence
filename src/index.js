7261fe8bc85aaff3956b4ecebf6e89db07d7376c
ceelogre <sam.nishimwe@yahoo.com>
2020-09-05 23:17:04 +0200

    ch: update dependencies
    ch: Configure CSR routing properly
    
    ch: Bump up dependencies

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers/root'
-import App from './app'
+import Paths from './paths'
+import {BrowserRouter} from 'react-router-dom'
 
 const store = createStore(rootReducer)
 
 render (
     <Provider store = {store} >
-        <App />
+        <BrowserRouter>
+            <Paths />
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 )
