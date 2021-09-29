64b28ec4fd5b3c8291d6c7f19f1582ab5def8ecb
ceelogre <sam.nishimwe@yahoo.com>
2021-09-29 14:58:56 +0200

    feat: add 404

--- src/paths.js
+++ src/paths.js
@@ -2,16 +2,21 @@ import React from 'react'
 import App  from './app'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Thoughts from './components/thoughts/index.js'
+import Work from './components/work/index'
 import ChangingRemote from './articles/changing-remote'
 import Header from './components/header'
+import fourOfour from './components/404'
 const Paths = () => {
     return (
         <Router>
             <Header />
             <Switch>
-                <Route path='/' exact component={App} />
+                <Route path='/thoughts/git-remote'   component={ChangingRemote} />
                 <Route path='/thoughts' component={Thoughts} />
-                <Route path='/articles/git-remote'   component={ChangingRemote} />
+                <Router path='/work' component={Work} />
+                <Route path='/' exact component={App} />
+                <Route component={fourOfour} />
+
             </Switch>
         </Router>
     )
