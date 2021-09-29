0cdf2a1cfa727bffccdd11a9acdc97df2429a1b9
ceelogre <sam.nishimwe@yahoo.com>
2021-09-29 14:58:56 +0200

    feat: add 404

---
+++ src/components/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class fourOfour extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
+    )
+  }
+}
+
+export default fourOfour
