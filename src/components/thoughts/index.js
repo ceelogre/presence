0609b1abfcaeee75ae181e74ed5d545ddac86eae
ceelogre <sam.nishimwe@yahoo.com>
2021-10-13 20:27:23 +0200

    WIP: add work section

--- src/components/thoughts/index.js
+++ src/components/thoughts/index.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link , Switch, Route} from 'react-router-dom'
+import ChangingRemote from '../../articles/changing-remote'
 
 // create a class declaration
 class Thoughts extends React.Component {
   // render method
   render() {
     return (
-      <div className="thoughts">
+      <main className="thoughts">
         <h3>Thoughts</h3>
         <p>
           Welcome to my Thoughts section. Here, You'll find anything from tech to trading to health. Enjoy!
         </p>
         <section>
-          <Link to="/articles/2>1">Bash output manipulation</Link>
-          <Link to="/articles/git-remote">Changing the remote </Link>
-          <Link to='/articles/heroku'> Heroku </Link>
-          <Link to='/articles/radar'>Traffic Police Radar</Link>
-          <Link to='/articles/se'>Stock Exchange</Link>
-          <Link to='/articles/git-rebase'>Rebasing</Link>
+          <Link to="/thoughts/2>1">Bash output manipulation</Link>
+          <Link to="/thoughts/git-remote">Changing the remote </Link>
+          <Link to='/thoughts/heroku'> Heroku </Link>
+          <Link to='/thoughts/radar'>Traffic Police Radar</Link>
+          <Link to='/thoughts/se'>Stock Exchange</Link>
+          <Link to='/thoughts/git-rebase'>Rebasing</Link>
           
         </section>
-      </div>
+        <Switch>
+
+               ./ <Route path='/thoughts/git-remote'   component={ChangingRemote} />
+        </Switch>
+      </main>
     );
   }
 }
---
+++ src/components/work/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class WorkIndex extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to the Work section</h1>
+        <p>Here, you'll find links to my work.</p>
+      </div>
+    )
+  }
+}
+
+export default WorkIndex
