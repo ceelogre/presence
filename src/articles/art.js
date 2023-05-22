7ebed14ae7ce959f59e73caca3d974697d11ca5d
ceelogre <ceelogre@outlook.com>
2023-05-22 23:22:07 +0200

    WIP: trip to art

---
+++ src/articles/art.js
@@ -0,0 +1,16 @@
+const article = () => {
+    const text  = `
+    Often people talk about being adventurous and it's become kind of a buzzword. As you can imagine, I have also used the word on multiple occasions and recently, I had a chance to test how far I'm willing to go with this. I had a package in Kenya that could not be delivered to Rwanda by air so on a long weekend, I decided to drive from Kigali to Nairobi to pick it.
+    This was a completely new experience, I had never driven across a country before. I scouted for information on the road status, the best itineries I can take and so forth but it didn't add up. It felt unsafe; I just had to take a leap of faith that it feasible.
+
+    The distance was roughly 1200 km or about 700 miles. This is really nothing for folks in other places with good road conditions or those with massive land but given that to drive across Rwanda would take about 340km this meant that I was going to drive across Rwanda up to four times.   
+
+    I learned a lot in this journey that's why I thought it's worth sharing. I did the basics; gased up the car, changed oil (yeah, some of us with old cars still do that) and a few more checks and I was off!
+    The best itinary according to Google Maps was Kigali - Katuna border Post - Mbarara - Masaka - Kampala - Jinja - Torroro - Eldoret - (city I don't remember) - Gilgil - Nairobi. I took a detour but we'll get to that later.
+
+    Crossing the border to Uganda was pretty easy as I've done it before the only problem is that I got to Kabale (the next town) in the evening which meant I was gonna drive to Kampala through the night something that I was really discouraged not to do but I did it anyway. On my way to Kampala, I noticed the change in gas price; as it turns out, the closer you get to town, the better the prices get so, places that are far from the capital have relatively higher gas price so I learned that I probably don't want to refill only in the main cities.
+
+    The next
+
+     `
+}
