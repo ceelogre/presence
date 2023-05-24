078c4ad83dfcd5556b2855a6f2c4cc595bb85f39
ceelogre <ceelogre@outlook.com>
2023-05-24 13:26:37 +0200

    feat: favorite song from each artist

---
+++ src/articles/fav.js
@@ -0,0 +1,13 @@
+const favorites = () => {
+    const title = "I'm making a collection of songs. For each artist I have to find my favorite song among all their songs even if they have over 1000"
+    const favs = [
+        {
+            artist: "Mike Will Made it",
+            Song: "23"
+        },
+        {
+            artist: "Post Malone",
+            Song: "Candy Paint"
+        }
+    ]
+}
