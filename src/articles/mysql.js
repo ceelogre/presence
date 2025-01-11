669dfc4359d9efac5760fc1c23c7be46be49c402
ceelogre <sam.nishimwe@yahoo.com>
2025-01-11 16:00:19 +0200

    feat(WIP): add mysql article

---
+++ src/articles/mysql.js
@@ -0,0 +1,13 @@
+const MysqlArticle = () => {
+    return (
+        <div>
+            <p>For long I've had an on and off relationship with mysql.</p>
+            <p>I could not connect to a new server</p>
+            <p>mysql -vh hostip -P port (3306) -u user -p</p>
+            <p>Connection was refused so I resorted to the last option. </p>
+            <p>sudo ufw allow 3306</p>
+            <p>telnet ip 3306</p>
+            <p>Now the host could be reached.</p>
+        </div>
+    )
+}
