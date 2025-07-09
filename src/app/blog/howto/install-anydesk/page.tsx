b4cfe06f07ac1e6aa8aafbfcd2e8af929e2944e1
ceelogre <sam.nishimwe@yahoo.com>
2025-07-09 15:15:57 +0200

    feat: finish anydesk article

--- src/app/blog/howto/install-anydesk/page.tsx
+++ src/app/blog/howto/install-anydesk/page.tsx
@@ -1,52 +1,62 @@
 import styles from './anydesk.module.css';
+import TerminalBlock from '@/components/TerminalBlock'
 
-export default function AptArticle() {
+export default function AnyDeskArticle() {
+    const addKeyCommand = `wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -`
+    const addKeyCommandSecure = `wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/anydesk-archive-keyring.gpg > /dev/null`
+    const addSourceCommand = `echo "deb [signed-by=/usr/share/keyrings/anydesk-archive-keyring.gpg] https://deb.anydesk.com/ all main&quot; | sudo tee /etc/apt/sources.list.d/anydesk-stable.list`
+    const updateCommand = `sudo apt update`
+    const installCommand = `sudo apt install anydesk`
+    const purgeCommand = `sudo apt purge anydesk`
+    const removeCommand = `sudo rm /etc/apt/sources.list.d/anydesk-stable.list`
+    const removeKeyCommand = `sudo rm /usr/share/keyrings/anydesk-archive-keyring.gpg`
     return (
         <div className={styles.container}>
-            <h1>How to install a software with apt package manager</h1>
-            <p>Most softwares are avaiable for linux; you just need to know the right way to install them. One of the quickest way to do it is by using a repository manager like apt for .deb and yum/dnf for .rpm</p>
+            <h1>How to install Anydesk without apt package manager</h1>
+            <p>Most softwares are available for linux; you just need to know the right way to install them. One of the quickest way to do it is by using a repository manager like apt for .deb and yum/dnf for .rpm</p>
             <p>In this demo, I&apos;m going to install Anydesk.</p>
-            <p>The first thing to do is to add the software fingerprint which allows the operating system to verify the authenticity of the publisher and many other things I do not know.</p>
+            <p>Anydesk is a remote desktop software that allows you to control your computer from another device. It&apos;s available for Linux, Windows, and Mac but the problem is that it&apos;s not available in the default repositories for all distributions (after all they have limited resources and there's only so much time/effort they can allocate to supporting every distro in existence).</p>
+            <p>The first thing to do is to add the software key which allows the operating system to verify the authenticity of the publisher and many other things I do not know.</p>
             <p>You may see online a lot of lines like this: </p>
-            <pre className={styles.code}>
-                <code>echo &quot;deb http://deb.anydesk.com/ all main&quot; | sudo tee /etc/apt/sources.list.d/anydesk-stable.list</code>
-            </pre>
-            <p>However, apt-key is now depreceated so this might cause you problems in the future. Use this:</p>
-            <pre className={styles.code}>
-                <code>wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | gpg --dearmor | sudo tee /usr/share/keyrings/anydesk-archive-keyring.gpg &gt; /dev/null</code>
-            </pre>
+            <TerminalBlock>
+                {addKeyCommand}
+            </TerminalBlock>
+            <p>However, apt-key is now depreceated so this might cause you problems in the future. Use this instead:</p>
+            <TerminalBlock>
+               {addKeyCommandSecure}
+            </TerminalBlock>
             <p>This will connect to anydesk server, download the keyring and add it in /usr/share/keyrings while at the sametime discarding the output (tee effect) so you don&apos;t see it on stdout (most likely your terminal)</p>
-            <p>After this, add the server to the sources:</p>
-            <pre className={styles.code}>
-                <code>echo &quot;deb [signed-by=/usr/share/keyrings/anydesk-archive-keyring.gpg] https://deb.anydesk.com/ all main&quot; | sudo tee /etc/apt/sources.list.d/anydesk-stable.list</code>
-            </pre>
+            <p>After this, add the server to the sources so that the package manager knows where to look:</p>
+            <TerminalBlock>
+                {addSourceCommand}
+            </TerminalBlock>
             <p>You can see that we explicitly add where to find the keyring before specifying what url to hit</p>
             <p>After this, update the repo manager so that it hits the new server</p>
-            <pre className={styles.code}>
-                <code>sudo apt update</code>
-            </pre>
+            <TerminalBlock>
+                {updateCommand}
+            </TerminalBlock>
             <p>If you inspect the output, there should be an entry for anydesk that&apos;s similar to</p>
-            <pre className={styles.code}>
-                <code>Hit:2 http://deb.anydesk.com all InRelease</code>
-            </pre>
+            <TerminalBlock>
+                Hit:2 http://deb.anydesk.com all InRelease
+            </TerminalBlock>
             <p>From here, install anydesk</p>
-            <pre className={styles.code}>
-                <code>sudo apt install anydesk</code>
-            </pre>
+            <TerminalBlock>
+                {installCommand}
+            </TerminalBlock>
             <p>Now the package manager will know where to look for this package and install it.</p>
-            <h3>Bonus</h3>
+            <h2>Bonus</h2>
             <p>If you ever need to uninstall it, you can do so with</p>
-            <pre className={styles.code}>
-                <code>sudo apt purge anydesk</code>
-            </pre>
+            <TerminalBlock>
+                {purgeCommand}
+            </TerminalBlock>
             <p>Remove the entry from sources.list</p>
-            <pre className={styles.code}>
-                <code>sudo rm /etc/apt/sources.list.d/anydesk-stable.list</code>
-            </pre>
+            <TerminalBlock>
+                {removeCommand}
+            </TerminalBlock>
             <p>If for some reason the GPG key is not removed automatically, you can also remove it manually</p>
-            <pre className={styles.code}>
-                <code>sudo rm /usr/share/keyrings/anydesk-archive-keyring.gpg</code>
-            </pre>
+            <TerminalBlock>
+                {removeKeyCommand}
+            </TerminalBlock>
             <p>This will remove the package and all its dependencies.</p>
         </div>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
-import ThemeToggle from "@/components/ThemeToggle";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -51,7 +50,6 @@ export default function RootLayout({
               {children}
             </main>
             <Footer />
-            <ThemeToggle />
           </div>
         </ThemeProvider>
       </body>
