1308465ccc4b5e4bd8fee2238f92f0b33ab9b708
ceelogre <sam.nishimwe@yahoo.com>
2026-01-01 23:54:28 +0200

    fix: use effect for date to bypass static builds

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,20 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+
 export default function Footer() {
+  const [year, setYear] = useState<number | null>(null)
+
+  useEffect(() => {
+    setYear(new Date().getFullYear())
+  }, [])
+
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="copyright">
-          <span>© {new Date().getFullYear()} Samuel Nishimwe. All rights reserved.</span>
+          <span>© {year} Samuel Nishimwe. All rights reserved.</span>
         </div>
         <div className="footer-message">
             <span>Made with ❤️ in Kigali</span>
