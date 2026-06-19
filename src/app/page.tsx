fc1db93a25fbbf85ba04ea3da3f32dbd07bf4530
Samuel Nishimwe <ceelogre@outlook.com>
2026-06-19 12:42:05 +0200

    feat: update header title (#173)
    * feat: update header title
    
    * feat: update header paragraph
    
    * ch: update packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div className={styles.home}>
       <section className={styles.hero}>
         <h1>Samuel Nishimwe</h1>
-        <h2>I enjoy being baffled by computers and being amazed at what human brains can accomplish</h2>
-        <p className={styles.subtitle}>Software Engineer & Technical Writer</p>
+        <p className={styles.subtitle}>Product & Software Engineer </p>
+        <h2>I enjoy getting baffled by computers and being amazed at what human brains can accomplish</h2>
         <p className={styles.description}>
-          Hi there, I&apos; m Sam, a developer from Kigali, Rwanda. Nice to meet you :) 
-          I build software solutions and write about technology, focusing on web development,
+          Hi there, I&apos; m Sam, an engineer from Kigali, Rwanda. Nice to meet you :) 
+          I build software solutions, lead teams, drive products and write about technology focusing on web development,
           software architecture, and developer experience.
           This is my personal website, here you can find my porfolio and if you have a minute, I also write about my non professional life.
         </p>
